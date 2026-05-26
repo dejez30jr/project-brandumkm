5bc782ce274b0108f17666670991e67a93f4d3af
Dev <dev@brandumkm.com>
2026-05-26 08:34:25 +0000

    fix 🐛: add submit and cancel button labels and remove create another

--- app/Filament/Resources/UmkmStikerResource/Pages/CreateUmkmStiker.php
+++ app/Filament/Resources/UmkmStikerResource/Pages/CreateUmkmStiker.php
@@ -9,4 +9,21 @@ use Filament\Resources\Pages\CreateRecord;
 class CreateUmkmStiker extends CreateRecord
 {
     protected static string $resource = UmkmStikerResource::class;
+
+    public static function canCreateAnother(): bool
+    {
+        return false;
+    }
+
+    protected function getCreateFormAction(): \Filament\Actions\Action
+    {
+        return parent::getCreateFormAction()
+            ->label('Submit');
+    }
+
+    protected function getCancelFormAction(): \Filament\Actions\Action
+    {
+        return parent::getCancelFormAction()
+            ->label('Cancel');
+    }
 }
